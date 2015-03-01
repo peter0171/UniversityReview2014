29c80a0fe0c615a1350bb988a049679c00cde7c7
peter0171 <peterL26@hotmail.com>
2015-03-01 11:27:12 -0800

    Upload UniversityReview

---
+++ UniversityReview/UniversityReview/Controllers/ReviewsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityReview.Models;
+
+namespace UniversityReview.Controllers
+{  
+    public class ReviewsController : Controller
+    {
+        UniRatingDB _db = new UniRatingDB();
+        //
+        // GET: /Reviews/
+
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var university = _db.Universities.Find(restaurantId);
+            if (university != null)
+            {
+                return View(university);
+            }
+            return HttpNotFound();
+        }
+
+        [HttpGet]
+        public ActionResult Create(int UniversityId)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(UniversityReviews review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.UniversityId });
+            }
+            return View(review);
+        }
+        [HttpPost]
+        public ActionResult Edit(UniversityReviews review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(review).State = EntityState.Modified;
+
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.UniversityId });
+            }
+            return View(review);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = _db.Reviews.Find(id);
+            return View(model);
+        }
+          protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UniversityReview/UniversityReview/Migrations/Configuration.cs
@@ -0,0 +1,81 @@
+namespace UniversityReview.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using System.Web.Security;
+    using UniversityReview.Models;
+    using WebMatrix.WebData;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UniversityReview.Models.UniRatingDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(UniversityReview.Models.UniRatingDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            context.Universities.AddOrUpdate(r => r.Name,
+          new University { Name = "Flinders University", City = "Adelaide" },
+          new University { Name = "Adelaide University", City = "Adelaide" },
+          new University { Name = "Melbourne University", City = "Adelaide" },
+          new University { Name = "Canberra University", City = "Canberra" },
+     
+          new University
+          {
+              Name = "Sydney University",
+              City = "Sydney",
+
+              Reviews =
+                  new List<UniversityReviews> { 
+                       new UniversityReviews { Rating = 95, Body="Great University", ReviewerName="peter" }
+                   }
+          }
+          
+          );
+
+           SeedMembership();
+
+
+        }
+
+        private void SeedMembership()
+        {
+            if (!WebSecurity.Initialized)
+            {
+                WebMatrix.WebData.WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            }
+            var roles = (SimpleRoleProvider)Roles.Provider;
+            var membership = (SimpleMembershipProvider)Membership.Provider;
+
+            if (!roles.RoleExists("Admin"))
+            {
+                roles.CreateRole("Admin");
+            }
+            if (membership.GetUser("mia", false) == null)
+            {
+                membership.CreateUserAndAccount("mia", "password");
+            }
+            if (!roles.GetRolesForUser("mia").Contains("Admin"))
+            {
+                roles.AddUsersToRoles(new[] { "mia" }, new[] { "Admin" });
+            }
+
+        }
+    }
+}
